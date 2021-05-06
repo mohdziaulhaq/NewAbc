33fe6fb5b0dbda72ea2b41301d590eab0bec6e96
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2021-05-06 16:25:26 +0530

    backend change

--- src/main/java/com/cg/service/LoginServiceInterface.java
+++ src/main/java/com/cg/service/LoginServiceInterface.java
@@ -6,10 +6,10 @@ import com.cg.entity.Engineer;
 import com.cg.exception.InvalidCredentialsException;
 
 public interface LoginServiceInterface {
-	Admin loginAdmin(int adminId,String password) throws InvalidCredentialsException;
-	Engineer loginEngineer(int employeeId,String password) throws InvalidCredentialsException;
-	Client loginClient(String clientId,String password) throws InvalidCredentialsException;
-	Admin logoutAdmin(Admin a) throws InvalidCredentialsException;
-	Engineer logoutEngineer(Engineer e) throws InvalidCredentialsException;
-	Client logoutClient(Client c) throws InvalidCredentialsException;
+	Admin loginAdmin(int adminId,String password);
+	Engineer loginEngineer(int employeeId,String password);
+	Client loginClient(String clientId,String password) ;
+//	Admin logoutAdmin(Admin a) ;
+//	Engineer logoutEngineer(Engineer e);
+//	Client logoutClient(Client c);
 }
